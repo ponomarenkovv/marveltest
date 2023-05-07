b84e2758fec0ecab52126f85735e0d60bc584f09
Vitaliy Ponomarenko <ponomarenkovitaliy@ukr.net>
2023-05-07 18:22:25 +0300

    add MarvelTest

---
+++ src/components/app/App.js
@@ -0,0 +1,44 @@
+import { Component } from "react/cjs/react.production.min";
+import AppHeader from "../appHeader/AppHeader";
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+import ErrorBaundary from "../errorBaundary/ErrorBaundary";
+
+import decoration from '../../resources/img/vision.png';
+
+class App extends Component {
+    state = {
+        selectedChar: null
+    }
+
+    onCharSelected = (id) => {
+        this.setState({
+            selectedChar: id
+        })
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <ErrorBaundary>
+                        <RandomChar/>
+                    </ErrorBaundary>
+                    <div className="char__content">
+                        <ErrorBaundary>
+                            <CharList onCharSelected={this.onCharSelected}/>
+                        </ErrorBaundary>
+                        <ErrorBaundary>
+                            <CharInfo charId={this.state.selectedChar}/>
+                        </ErrorBaundary>
+                    </div>
+                    <img className="bg-decoration" src={decoration} alt="vision"/>
+                </main>
+            </div>
+        )
+    }   
+}
+
+export default App;
---
+++ src/components/errorBaundary/ErrorBaundary.js
@@ -0,0 +1,24 @@
+import { Component } from "react/cjs/react.production.min";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+
+
+class ErrorBaundary extends Component {
+    state = {
+        error: false
+    }
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({
+            error: true
+        })
+    }
+
+    render() {
+        if (this.state.error) {
+            return <ErrorMessage/>
+        }
+        return this.props.children;
+    }
+}
+
+export default ErrorBaundary;
